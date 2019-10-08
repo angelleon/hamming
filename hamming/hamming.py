e4f6c6199cddda38b49327071c2ea3c7c4136b61
Angel Leon <luianglenlop@gmail.com>
2019-10-08 12:30:06 -0500

    terminado algoritmo para crear mensaje

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -6,16 +6,18 @@ import struct
 from .ui import Application
 from .receptor import Receptor
 
-def es_potencia_2(a):
+
+def is_2_pow(a):
     if a == 1:
         return True
     while True:
         if a % 2 == 0:
             a //= 2
+            if a == 1:
+                return True
         else:
             return False
-        if a == 1:
-            return True
+
 
 def gen_mask(pwr, length):
     base = 1
@@ -37,29 +39,85 @@ def gen_mask(pwr, length):
         i += 1
     return mask
 
+
+def insert_redundant_bits(data):
+    print(f"data: {bin(data)[2:]}")
+    if data == 0:
+        return bytearray(5), "0" * 32
+    #                      5        4        3        2        1
+    #               87654321 87654321 87654321 87654321 87654321
+    parity_bits = 0b00000000_00000000_00000000_00000000_00000000
+    for i in range(6):
+        ones = 0
+        mask = gen_mask(2**i, 32)
+        masked = data & mask
+        print(f"data:  {bin(data)[2:]}")
+        print(f"mask:  {bin(mask)[2:]}")
+        print(f"masked:{bin(masked)[2:]}")
+        while masked != 0:
+            if masked & 1 == 1:
+                ones += 1
+            masked >>= 1
+        print(f"ones: {ones}")
+        print(f"parity_bits: {bin(parity_bits)}")
+        if ones % 2 == 0:
+            parity_bits |= 1 << (2**i - 1)
+        else:
+            parity_bits |= abs(~(1 << (2**i - 1)))
+        print(f"parity_bits: {bin(parity_bits)}")
+    i = 1
+    ret_data = parity_bits
+
+    while data != 0:
+        if not is_2_pow(i):
+            ret_data |= (data & 1) << i
+            data >>= 1
+        i += 1
+        print(f"ret_data: {bin(ret_data)}")
+    data = ret_data
+    data_str = bin(data)[2:]
+    ret_data = bytearray()
+    for i in range(5):
+        ret_data += struct.pack("B", data & 0xff)
+        data >>= 8
+        print(f"ret_data: {repr(ret_data)}")
+    return ret_data, data_str
+
+
 def hamming_algorithm(data: bytearray):
+    """Funcion que agrega los bits de comprobacion antes de enviarse en mensaje"""
+    print(f"data: {data}")
     #        12345678 12345678 12345678 12345678
     if len(data) % 4 != 0:
         diff = 4 - len(data) % 4
         data += bytes(diff)
-    m = len(data) * 4
+    m = len(data)
     r = 0
-    while 2 ** r < m + r + 1:
-        r += 1
+    chunck = 0
     i = 0
+    ret_data = bytearray()
+    while i < m // 4:
+        chunck = struct.unpack("I", data[i*4:i*4 + 4])[0]
+        chunck, chunck_str = insert_redundant_bits(chunck)
+        print(f"chunck: {chunck} chunck_str:{chunck_str}")
+        i += 1
+
+def chk_hamming(data):
+    """Funcion quer comprueba el mensaje que se recibe"""
+    pass
 
 
-
-
-
-
-
-
-
-class Hamming:
+class Hamming(Application):
     def __init__(self):
-        self.ui = Application()
+        super().__init__()
         self.connection = Receptor
+        self.connection = Emisor()
+        #self.w.
+
+    def toggle_mode(self):
+
+    def send(self):
+        pass
 
     def start(self):
         return self.ui.exec()
--- test_hamming.py
+++ test_hamming.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 
-from hamming.hamming import gen_mask
+from struct import pack
 
-gen_mask(2, 40)
+from hamming.hamming import gen_mask, insert_redundant_bits, hamming_algorithm
+
+
+def str_to_bytearray(s):
+    arr = bytearray()
+    for c in s:
+        b = ord(c)
+        arr += pack("B", b)
+    return arr
+
+
+"""gen_mask(2, 40)
 gen_mask(4, 40)
 gen_mask(8, 40)
 gen_mask(16, 40)
 
+insert_redundant_bits(0xff_ff_ff_ff)
+# insert_redundant_bits(0x0f)
+# insert_redundant_bits(0x08)
+# insert_redundant_bits(0x04)"""
+
+print(hamming_algorithm(b"\xfe\xff\xff\xff"))
