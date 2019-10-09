2718f85c244bac9daeafe6a2c2e3af71c7256bb2
Angel Leon <luianglenlop@gmail.com>
2019-10-09 12:51:45 -0500

    connected events, added hamming construction

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python3
 
 from typing import Union
-import struct
+from struct import Struct
+from ipaddress import IPv4Address, AddressValueError
+from random import random, randrange
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QPlainTextEdit
 
 from .ui import Application
 from .receptor import Receptor
+
+struct = Struct("B")
+converter = Struct("I")
+
+
+def str_4_by_4(s):
+    ret = ""
+    if len(s) < 4:
+        return s
+    if len(s) % 4 != 0:
+        s += " " * (4 - len(s) // 4)
+    i = 0
+    while i < len(s) // 4:
+        if i % 4 == 0 and i != 0:
+            ret += "\n"
+        ret += s[i * 4: i * 4 + 4] + " "
+        i += 1
+    return ret
+
+def str_to_bytearray(s):
+    arr = bytearray()
+    for c in s:
+        b = ord(c)
+        arr += struct.pack(b)
+    return arr
 
 
 def is_2_pow(a):
@@ -78,7 +108,7 @@ def insert_redundant_bits(data):
     data_str = bin(data)[2:]
     ret_data = bytearray()
     for i in range(5):
-        ret_data += struct.pack("B", data & 0xff)
+        ret_data += struct.pack(data & 0xff)
         data >>= 8
         print(f"ret_data: {repr(ret_data)}")
     return ret_data, data_str
@@ -96,11 +126,16 @@ def hamming_algorithm(data: bytearray):
     chunck = 0
     i = 0
     ret_data = bytearray()
+    ret_str = ""
     while i < m // 4:
-        chunck = struct.unpack("I", data[i*4:i*4 + 4])[0]
+        chunck = converter.unpack(data[i * 4: i * 4 + 4])[0]
         chunck, chunck_str = insert_redundant_bits(chunck)
         print(f"chunck: {chunck} chunck_str:{chunck_str}")
+        ret_data += chunck
+        ret_str += chunck_str
         i += 1
+    return ret_data, ret_str
+
 
 def chk_hamming(data):
     """Funcion quer comprueba el mensaje que se recibe"""
@@ -108,17 +143,72 @@ def chk_hamming(data):
 
 
 class Hamming(Application):
+    CSS_ERR_QLINE_EDIT = "QLineEdit{background: DarkRed; color: LightGray}"
+    CSS_DEFAULT_QLINE_EDIT = "QLineEdit{}"
+
     def __init__(self):
         super().__init__()
         self.connection = Receptor
-        self.connection = Emisor()
-        #self.w.
+        # self.connection = Emisor()
+        self.w.btn_action.clicked.connect(self.slt_process_msg)
+        self.w.rdio_send.toggled.connect(self.slt_rdio_toggle_mode)
+        self.is_client = True
+        # self.w.txt_ip.setStyleSheet("QLineEdit{background: DarkRed; color: LightGray}")
 
-    def toggle_mode(self):
+    @pyqtSlot(bool)
+    def slt_rdio_toggle_mode(self, checked):
+        if checked:
+            self.is_client = True
+            self.w.txt_editor.setEnabled(True)
+            self.w.txt_ip.setEnabled(True)
+        else:
+            self.is_client = False
+            self.w.txt_editor.setEnabled(False)
+            self.w.txt_ip.setEnabled(False)
+
+    @pyqtSlot(bool)
+    def slt_process_msg(self):
+        if self.is_client:
+            if not (self.chk_ip() and self.chk_port()):
+                return
+            msg = self.w.txt_editor.toPlainText()
+            if len(msg) == 0:
+                return
+            msg = str_to_bytearray(msg)
+            msg, msg_str = hamming_algorithm(msg)
+            self.w.txt_show.setPlainText(str_4_by_4(msg_str))
+        else:
+            pass
+
+    @pyqtSlot(bytes)
+    def slt_recv_msg(self, msg):
+        pass
+
+    def chk_ip(self):
+        self.w.txt_ip.setStyleSheet(self.CSS_DEFAULT_QLINE_EDIT)
+        ip = self.w.txt_ip.text()
+        print(ip)
+        try:
+            IPv4Address(ip)
+        except AddressValueError:
+            self.w.txt_ip.setStyleSheet(self.CSS_ERR_QLINE_EDIT)
+            return False
+        return True
+
+    def chk_port(self):
+        self.w.txt_port.setStyleSheet(self.CSS_DEFAULT_QLINE_EDIT)
+        port = self.w.txt_port.text()
+        try:
+            port = int(port)
+            assert 0 <= port < 65536
+        except (ValueError, AssertionError):
+            self.w.txt_port.setStyleSheet(self.CSS_ERR_QLINE_EDIT)
+            return False
+        return True
 
     def send(self):
         pass
 
     def start(self):
-        return self.ui.exec()
+        return self.exec()
 
